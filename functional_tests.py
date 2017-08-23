df04b31a72e10186ed5dc506921e5906d1bbe9c2
Alex Ciccone <aciccone22@gmail.com>
2017-08-23 16:08:41 -0500

    First FT specced out (with user stories) in comments, and program now uses the unittest unit testing framework.

--- functional_tests.py
+++ functional_tests.py
@@ -1,17 +1,43 @@
 from selenium import webdriver
+import unittest
 
-# Starting a Selenium "webdriver" to pop up a real Firefox browser window
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-#browser.accept_untrusted_certs = True
-#browser.assume_untrusted_cert_issuer=True
-#browser.accept_next_alert = True
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Using it to open up a web page we're expecting to be served from local PC
-browser.get('http://localhost:8000')
+    def tearDown(self):
+        self.browser.quit()
 
-# This is our first functional test (FT)
-# Checking (making a test assertion) that page has the word "Django" in title
-# Note: assert raises an exception if a given condition isn't true
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith has heard about a cool new online to-do app. He goes to check
+        # out its homepage
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is
+        # tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do lists
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonder whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
