df1046a9f8bde631f52b95b67cc0abc7a1fe0863
Alex Ciccone <aciccone22@gmail.com>
2017-08-23 23:43:37 -0500

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -9,7 +9,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. He goes to check
         # out its homepage
         self.browser.get('http://localhost:8000')
